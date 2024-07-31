3c694b7d9ba76a4ef7fe0346d06f7cc8e2735c0f
limagl <carvalhoworkspace@gmail.com>
2024-07-31 18:45:37 -0300

    Adicionando operadores a minha calculadora

--- src/script.js
+++ src/script.js
@@ -1,11 +1,38 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(num1, num2, operador) {
+    let resultado 
+    let num1 = parseInt(num1)
+    let num2 = parseInt(num2)
 
+    switch (operador) {
+      case "+":
+        resultado = num1 + num2
+        break;
+      case "-":
+        resultado = num1 - num2
+        break;
+      case "/":
+        resultado = num1 / num2
+        break;
+      case "*":
+        resultado = num1 * num2 
+        break;
+      case "e":
+        let contador = 1
+        resultado = num1 * num1
+        while (contador < num2 - 1) {
+          resultado = resultado * num1
+          contador++
+        }
+        break;
+    }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    if (resultado == undefined || resultado > 1000000)
+      resultado = "ERRO"
+
+    return resultado
+}
+
 
 let visor = document.getElementById("visor");
 
